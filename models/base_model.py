8a7037d4bf3f2b6dda6667f90b789d49fe808154
root <gonzalocabaleiro12@gmail.com>
2023-02-22 13:53:06 -0800

    First commit for Task 3 to 5

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Module doc"""
+import uuid
+from datetime import datetime
+from models.__init__ import storage
+
+
+class BaseModel:
+    """ Class doc"""
+    def __init__(self, *args, **kwargs):
+        """ __init__ doc"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """ __str__ doc"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        """ save doc"""
+        storage.save()
+
+    def to_dict(self):
+        """ to_dict doc"""
+        self.__dict__.update({'__class__': self.__class__.__name__,
+                              'created_at': self.created_at.isoformat(),
+                              'updated_at': self.updated_at.isoformat()})
+        return self.__dict__
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module doc"""
+import json
+from os import path
+from datetime import datetime
+
+class FileStorage:
+    """ Class doc"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """ all doc"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ new doc"""
+        FileStorage.__objects[f'{obj.__class__.__name__}.{getattr(obj, "id")}'] = obj.__dict__
+
+    def save(self):
+        """ save doc"""
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
+            json.dump(FileStorage.__objects, f, default=str)
+
+    def reload(self):
+        """ reload doc"""
+        if not path.exists(FileStorage.__file_path):
+            return
+        try:
+            with open(FileStorage.__file_path, encoding='utf-8') as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    for k, v in value.items():
+                        if k in ['created_at', 'updated_at']:
+                            value[k] = datetime.fromisoformat(v)
+                FileStorage.__objects = data
+        except json.decoder.JSONDecodeError:
+            FileStorage.__objects = {}
